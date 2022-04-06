e6a1be987770b36c01d76cd30b645845ed8aebe3
Rafael Queiroz <rafa.qo@gmail.com>
2022-04-06 20:55:35 -0300

    adicionei primeiras funções no arquivo tasks.js

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,21 +1,40 @@
-onload = () => {
-
+document.addEventListener('load', function () {
     var API_URL = 'https://ctd-todo-api.herokuapp.com/v1';
 
-    var obterNomeUsuario = fetch(API_URL)
-    .then(respostaDoServidor => {
+    var obterNomeUsuario = fetch(`${API_URL}/users/getMe`)
+    .then(resultado => {
         // Primeira resposta do servidor. 
-        return respostaDoServidor.json();
-    })
-    .then(nomeUsuario => {
-
+        return resultado.json();
+    }
+    ).then(resultado => {
         // Resposta do resultado da resposta no formato JSON.
-        nomeUsuario = respostaDoServidor.GET("email");
-        return nomeUsuario;
+        resultadoEmJSON(resultado);
+    }
+    ).catch(
+        erros => {
+            console.log(erros);
+        }
+    );
 
-    });
+    var listarTodasTarefas = fetch(`${API_URL}/tasks`)
+    .then(resultado => {
+        // Primeira resposta do servidor.
+        return resultado.json();
+    }
+    ).then(resultado => {
+        // Resposta do resultado da resposta no formato JSON.
+        resultadoEmJSON(resultado);    
+    }
+    ).catch(
+        erros => {
+            console.log(erros);
+        }
+    );
 
 
-};
+    
+})
 
- 
+        // let nome = document.getElementsByClassName("user-info");
+        // nome[0].createTextNode(nomeUsuario);
+        // p.appendChild.nome
